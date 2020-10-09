4f79a6647a50092dd73e301ffdff69668a0b4fbf
EagerBeaver98 <jooke98@outlook.com>
2020-10-09 22:09:08 +0000

    Loops

--- loopy-lighthouse.js
+++ loopy-lighthouse.js
@@ -1,7 +1,11 @@
 for (let num = 100; num < 201; num++){ //counter
-  if(num % 3 === 0){ //remander checker for 3
-    console.log("Loopy")
-  }else {
+  if(num % 3 === 0 && num % 4 === 0){ //remander checker for 3
+    console.log("LoopyLighthouse")
+  }else if (num % 3 === 0){ 
+    console.log('Loopy')
+  }else if (num % 4 === 0){
+    console.log('Lighthouse')
+  }else{
     console.log(num)
-  } 
+  }
 }
