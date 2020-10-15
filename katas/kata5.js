17b405bed9c8156ce5783d133d49c9b5277e6447
vagrant <jooke98@outlook.com>
2020-10-15 20:49:55 +0000

    Katas in progress

--- katas/kata5.js
+++ katas/kata5.js
@@ -1,9 +1,16 @@
 const urlEncode = function (text){
-  if (text[0] === ' '){
-    
+  let temp = ''
+  for (let x = 0; x < text.length; x++){
+    if (text[x] !== ' '){
+      temp += text[x]
+    }else if (x === 0 || x === (text.length - 1))
+      temp = temp
+    else if (text[x] === ' '){
+      temp += '%20'
+    }
   }
+  return temp
 }
 console.log(urlEncode("Lighthouse Labs"));
 console.log(urlEncode(" Lighthouse Labs "));
 console.log(urlEncode("blue is greener than purple for sure"));
-
