0ce61014aff58f5fc131b7be7bc14b5652ef4826
EagerBeaver98 <jooke98@outlook.com>
2020-10-21 16:03:19 +0000

    Kata in progress

--- katas/kata9.js
+++ katas/kata9.js
@@ -12,3 +12,40 @@ const prismVolume = function (height, width, depth){
   return height * width * depth
 }
 
+const totalVolume = function (solids){
+  let total = 0
+  for (let x = 0; x <= solids.length; x++){
+    if (solids['type'] === 'sphere'){
+      total += sphereVolume(solids['radius'])
+    }else if (solids['type'] === 'cone'){
+      total += coneVolume(solids['radius'], solids['height'])
+    }else if (solids['type'] === 'prism'){
+      total += prismVolume(solids['height'], solids['width'], solids['depth'])
+    }
+  }
+  return total
+}
+
+const largeSphere = {
+  type: 'sphere',
+  radius: 40
+}
+
+const smallSphere = {
+  type: 'sphere',
+  radius: 10
+}
+
+const cone = {
+  type: 'cone',
+  radius: 3,
+  height: 5
+}
+
+const duck = [
+  largeSphere,
+  smallSphere,
+  cone
+]
+
+console.log(272000 < totalVolume(duck) && totalVolume(duck) < 275000);
